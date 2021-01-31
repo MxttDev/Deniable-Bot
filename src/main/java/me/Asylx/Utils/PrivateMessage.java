4ab41a2988682131d9f56f8ad67aabd0e25a74ba
Matt <37265551+MattyTheDeveloper@users.noreply.github.com>
2021-01-31 15:22:09 +0000

    Add files via upload

--- src/main/java/me/Asylx/Utils/PrivateMessage.java
+++ src/main/java/me/Asylx/Utils/PrivateMessage.java
@@ -1,5 +1,16 @@
 package me.Asylx.Utils;
 
+import net.dv8tion.jda.api.entities.User;
+
 public class PrivateMessage {
+
+    public static void sendPrivateMessage(User user, String content) {
+        // openPrivateChannel provides a RestAction<PrivateChannel>
+        // which means it supplies you with the resulting channel
+        user.openPrivateChannel().queue((channel) ->
+        {
+            channel.sendMessage(content).queue();
+        });
+    }
 
 }
